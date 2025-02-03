11fa764ce8996dcfc71fa8c9a1fad094184fd125
iam-prabha <dev.vpcode@gmail.com>
2025-02-03 19:41:47 +0530

    list and tuple additional

--- python/Lists.py
+++ python/Lists.py
@@ -8,7 +8,11 @@
 # string is immutable trigger err like typeErr
 
 # list methods
-# append() => concat
+# append() => concat or add new element in list
+# pop() => remove last element in list or remove element by index like pop(1).
+# remove() => remove element by value like remove('banana') &remove only first occurance
+# insert() => insert element by index like insert(1, 'kiwi')
+# extennd() => add list to another list like list1.extend(list2)
 # sort() => sorted by in order
 # built-in function and list
 # len() => returns total length start in 1...n-1
@@ -16,5 +20,37 @@
 # min(value) => find min value in list
 # sum(value) => sum all from the Firstindex to lastIndex
 
-# lists vs strings
+# SAMPLE CODE
+# lst = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango']
+# index : 0,        1,      2,           3,     4,      5,        6
+# index : -7,      -6,     -5,         -4,    -3,     -2,      -1 - negative index eg: -1 is mango.-2 is melon
 
+# NESTED LIST SAMPLE CODE
+# lst = [
+#     [1, 2, 3],
+#     [4, 5, 6]
+# ]
+# print(lst[0][1][0])
+
+#defalut list parameter not recommended way to use
+# def func(lst = []):
+#     lst.append(1)
+#     print(lst)
+
+# silicing list
+# lst [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(lst[2:5]) # [3, 4, 5] not include 5 index with single colon
+# print(lst[1:5:2]) # start, end, step [2, 4] with double colon but end index not include
+# print(lst[::-1]) # reverse list [9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+# zip() => combine two list of create as tuple
+# names = ['apple', 'banana', 'cherry']
+# ages = [1, 2, 3]
+# zipped = zip(names, ages)
+# print(list(zipped)) # [('apple', 1), ('banana', 2), ('cherry', 3)]
+
+# List Comprehension
+# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# lst = [x for x in range(10) if x % 2 == 0] => [0, 2, 4, 6, 8]
+# lst = [[x for x in range(3)] for _ in range(3)] => [[0, 1, 2], [0, 1, 2], [0, 1, 2]].
+# print(lst)
--- python/tuples.py
+++ python/tuples.py
@@ -1,14 +1,15 @@
 # Tuples are like lists 
 # - have elements which are indexed started at 0
-# - its immutable and contents
+# - its a immutable and a contents
 # dont have sort(),append(),reverse() => traceback err:attr Err
 # has only - index,count 
 # operators work with tuples
 # sorting list of tuples
-d = {'a':20,'b':2,'c':34}
+# d = {'a':20,'b':2,'c':34}
 # t = sorted(d.items())
-
+d = {'a', 'b', 'c'}
 # for k,v in t :
 #    print(k,v)  
-print(c)
+# print(c)
 # sort by values instead of key
+print(tuple(d.item()))
