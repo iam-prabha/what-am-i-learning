6d579abe54bce8b34e7ad8c1c1e764b29b80fb43
iam-prabha <vel.prabhakaran.ds@gmail.com>
2025-05-28 22:28:36 +0530

    deep learning added

---
+++ web scraping/request_module.py
@@ -0,0 +1,11 @@
+import requests
+# making a GET request
+
+req = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
+
+# checking the status code
+# successful response 200
+print(req.status_code)
+print('\n')
+# checking the content of the response
+print(req.content)
